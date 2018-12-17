3c4a273c8f108d5ff0d1028e174b15ce19fc50b0
K Y T 3 C <gonzalo.garcia@copernic.cat>
2018-12-17 16:55:54 +0100

    Añadido a onCreate(): SCREEN ORIENTATION PORTAIT y FULLSCREEN

--- app/src/main/java/cat/copernic/kyt3c/fitnessreference/MainActivity.java
+++ app/src/main/java/cat/copernic/kyt3c/fitnessreference/MainActivity.java
@@ -1,13 +1,21 @@
 package cat.copernic.kyt3c.fitnessreference;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
     }
 }
