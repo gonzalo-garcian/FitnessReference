99293864f9a0aa20f397d8e6884698024be498bf
DESKTOP-OT46VKV\Gonzalo <kyt3c.inphyx@gmail.com>
2019-01-28 21:36:02 +0100

    Menu funcional

--- app/src/main/java/cat/copernic/kyt3c/fitnessreference/menu.java
+++ app/src/main/java/cat/copernic/kyt3c/fitnessreference/menu.java
@@ -1,15 +1,19 @@
 package cat.copernic.kyt3c.fitnessreference;
 
 import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
 public class menu extends AppCompatActivity {
 
     // MENU TEMPORAL DE GONZALO
-
+    private DrawerLayout drawer;
     Button btnRecicler;
     Button btnCalendar;
     @Override
@@ -17,8 +21,26 @@ public class menu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
         btnRecicler = findViewById(R.id.btnRecicler);
         btnCalendar = findViewById(R.id.btnCalendar);
+    }
+
+    @Override
+    public void onBackPressed(){
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
     }
 
     public void recicler(View view) {
