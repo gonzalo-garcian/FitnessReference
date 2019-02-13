0ba0475b64312c95deb109b9a97cfa338a8e1cde
Gonzalo <gonzalo.garcia@copernic.cat>
2019-02-13 12:54:41 +0100

    Menu ahora funciona con fragments , creado fragment de prueba.

--- app/src/main/java/cat/copernic/kyt3c/fitnessreference/menu.java
+++ app/src/main/java/cat/copernic/kyt3c/fitnessreference/menu.java
@@ -1,6 +1,8 @@
 package cat.copernic.kyt3c.fitnessreference;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -11,12 +13,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class menu extends AppCompatActivity {
+public class menu extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    // MENU TEMPORAL DE GONZALO
     private DrawerLayout drawer;
-    Button btnRecicler;
-    Button btnCalendar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,12 +26,27 @@ public class menu extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.ic_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new MessageFragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
