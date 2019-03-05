d32ed0ea4cd57d671fbdc6ef97caadf003884004
RKJD <44412937+RKJD@users.noreply.github.com>
2019-03-05 13:31:17 +0100

    Actualizado el RecyclerView

--- app/src/main/java/cat/copernic/kyt3c/fitnessreference/RecyclerviewFragment.java
+++ app/src/main/java/cat/copernic/kyt3c/fitnessreference/RecyclerviewFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -37,6 +38,9 @@ public class RecyclerviewFragment extends Fragment implements MyRecyclerViewAdap
         adapter = new MyRecyclerViewAdapter(getContext(), actividades);
         adapter.setClickListener(this);
         recyclerView.setAdapter(adapter);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+                ((LinearLayoutManager) recyclerView.getLayoutManager()).getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
 
         return vista;
     }
