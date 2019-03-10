e57a360b75bce89519558e7e2a8e3fd314b4c595
DESKTOP-OT46VKV\Gonzalo <kyt3c.inphyx@gmail.com>
2019-03-10 19:37:56 +0100

    Añadido Activity Editar Perfil

---
+++ app/src/main/java/cat/copernic/kyt3c/fitnessreference/EditarPerfilFragment.java
@@ -0,0 +1,13 @@
+package cat.copernic.kyt3c.fitnessreference;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditarPerfilFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_perfil_fragment);
+    }
+}
